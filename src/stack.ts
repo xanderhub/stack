40dea752a0c7b9236f387095a9f178391a1db026
Alexander Ivanov <33380175+xanderhub@users.noreply.github.com>
2024-01-06 12:47:23 +0200

    test7_refactored

--- src/stack.ts
+++ src/stack.ts
@@ -3,27 +3,20 @@ import {Item} from "./item";
 
 export class Stack {
 
-    private _size: number;
     private _top?: Item;
 
-    constructor() {
-        this._size = 0;
-    }
-
     public isEmpty(): boolean {
-        return this._size == 0;
+        return !this._top;
     }
 
     public push(value: number): void {
         this._top = {value: value, next: this._top};
-        this._size ++;
     }
 
     public pop(): number {
         if (this.isEmpty())
             throw new StackError(StackError.popFromEmptyStackErrorMessage);
 
-        this._size --;
         let value: number = this._top?.value as number;
         this._top = this._top?.next;
         return value;
